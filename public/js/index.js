355b8239661e26e81264229945b0d7904e5c1dfe
jhonalcala <jhon.alcala@reekoh.com>
2017-10-26 09:20:37 +0800

    Initial commit

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,12 @@ var socket = io();
 socket.on('connect', function(){
     console.log('Connection Success');
 
-   
 })
 
 socket.on('disconnect', function(){
     console.log('Disconnected from the server');
 })
 
-socket.on('email', function(email){
-    console.log('New Email', email)
+socket.on('newMessage', function(message){
+    console.log('New Message', message);
 })
